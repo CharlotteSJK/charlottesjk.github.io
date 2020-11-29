dbee327d364a858945997685417f284cf6d6f060
Charlotte Keddie <charlottekeddie@gmail.com>
2020-11-29 12:55:46 +0100

    Layout changes for mobile and fixed toggles for nav and portfolio

--- main.js
+++ main.js
@@ -3,10 +3,11 @@ const navLinks = document.querySelector('#nav-links');
 const navMenu = document.querySelector('.navigation');
 const face = document.querySelector('.face');
 const profile = document.querySelector('#profile');
-const plus = document.querySelector('.plus');
+const pluses = document.querySelector('.plus');
 const blurb = document.querySelector('.card-blurb')
 
 // Toggle between showing and hiding the navigation menu
+
 hamburger.addEventListener('click', e => {
   if (navLinks.style.display == "block") {
     navLinks.style.display = "none";
@@ -15,12 +16,13 @@ hamburger.addEventListener('click', e => {
   }
 })
 
-// document.addEventListener('click', e => {
-//     if (e.target != hamburger || navMenu)
-//     navLinks.style.display = "none";
-// })
+document.addEventListener('click', e => {
+    if (e.target != hamburger)
+    navLinks.style.display = "none";
+})
 
 //Makes the colors change on the profile image on hover
+
 face.addEventListener('mouseover', e => {
   if (e.target === face) {
     profile.src = "images/profile-circles-pinks-alt.png";
@@ -33,46 +35,20 @@ face.addEventListener('mouseout', e => {
   }
 })
 
-// console.log(plus);
+//Shows or hides the description for each project
 
-plus.addEventListener('click', e => {
-  if (blurb.style.display = "none") {
-    plus.innerHTML = "-";
+pluses.addEventListener('click', e => {
+  if (blurb.style.display == "none") {
+    pluses.innerHTML = "-";
     blurb.style.display = "block";
   } else {
     blurb.style.display = "none";
-    plus.innerHTML = "+";
+    pluses.innerHTML = "+";
 }})
 
-document.querySelectorAll('.plus').forEach(item => {
-  item.addEventListener('click', event => {
-    //handle click
-  })
-})
-
-// plus.forEach();
-
-// if (time < 10) {
-//   greeting = "Good morning";
-// } else if (time < 20) {
-//   greeting = "Good day";
-// } else {
-//   greeting = "Good evening";
-// }
-
-
-
-
-// search.addEventListener('keyup', () => {
-//   const searchTerm = search.value.toLowerCase();
-//   const employeeNames = document.querySelectorAll('.name');
-//   employeeNames.forEach(employeeName => {
-//     const text = employeeName.textContent.toLowerCase();
-//     const box = employeeName.parentElement.parentElement;
-//     if(text.indexOf(searchTerm) > -1) {
-//       box.style.display = "flex";
-//     } else {
-//       box.style.display = "none";  
-//     }
-// 	})
+// pluses.forEach(pluse => {
+//   pluse.addEventListner('click', (e)=>{
+//     console.log('someone hit me');
+//   });
 // });
+
